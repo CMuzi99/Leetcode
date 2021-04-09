688128232cec4474da5500ab357f6f47709dc6bd
Chengmuzi <43950653+CMuzi99@users.noreply.github.com>
2021-04-09 13:24:56 +0200

    Update FindNumberswithEvenNumberofDigits.java

--- Apr/FindNumberswithEvenNumberofDigits.java
+++ Apr/FindNumberswithEvenNumberofDigits.java
@@ -2,7 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int number = 0;
         for(int i=0; i<nums.length;i++){
-           String s = Integer.toString(nums[i]);
+           String s = Integer.toString(nums[i]);  //Integer.toString(i) transfer the int i to string.
             if(s.length()%2==0){number++;}
         }
         return number;
