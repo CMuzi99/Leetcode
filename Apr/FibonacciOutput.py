14228f87ff08f40edf3555f604beb8923fe637cc
Chengmuzi <43950653+CMuzi99@users.noreply.github.com>
2021-04-12 16:36:21 +0200

    斐波那契数列输出

---
+++ Apr/FibonacciOutput.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Fibonacci(self, n):
+        # write code here
+        if n<0:return 0
+        a=0
+        b=1
+        for i in range(n):
+            a,b = b,b+a 
+        return a
+    
