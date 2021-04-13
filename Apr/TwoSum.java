b0864e2e8022f611c8ac5d3bceed09937428195a
Muzi Cheng <43950653+CMuzi99@users.noreply.github.com>
2021-04-13 14:18:39 +0200

    Update TwoSum.java

--- Apr/TwoSum.java
+++ Apr/TwoSum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int i=0,j=1;
         for(i=0;i<nums.length-1;i++){
-            for(j=1;j<nums.length;j++){
+            for(j=i+1;j<nums.length;j++){   //I don't know why here j=i+1; I use j=1 then it's wrong
                 if (nums[i]==target-nums[j]){
                     return new int[]{i,j};
                 }
